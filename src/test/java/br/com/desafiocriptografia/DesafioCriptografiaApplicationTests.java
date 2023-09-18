025853c3d945bbe6ef204422c3a20fbc6d740b91
wallysoneves <wallysoneves.moraes@gmail.com>
2023-09-17 22:54:41 -0300

    pacotes de testes que irao ser implementados

---
+++ src/test/java/br/com/desafiocriptografia/DesafioCriptografiaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.desafiocriptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@SpringBootTest
+class DesafioCriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
